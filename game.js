0d8a6b01fddd4521ea01ed35c50be02f93f62878
Joshua Dulin <jzdulin@Joshuas-MBP-2.lan>
2018-05-20 16:51:00 -0400

    added functionality for winning

--- game.js
+++ game.js
@@ -1,9 +1,20 @@
 var wins = 0;
 var losses = 0;
-var guesses = 0;
+var guesses = 10;
 var guessed = [];
 var randomrWord = [];
 
 var words = ["random", "words", "to", "test"]
 var randomWord = words[Math.floor(Math.random() * words.length)]
 var randomWord_ = randomWord.replace(/[a-z]/g, '_ ')
+
+document.onkeyup = function(event) {
+
+    if (guesses === 0) {
+        losses++;
+    }
+    
+    if (computerword_ === computerword) {
+        wins++;
+    }
+}
